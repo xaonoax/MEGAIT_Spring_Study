17de9397a5dff1ed58605faead04f44189d4c1f3
Ji-0 <xaonoax@gmail.com>
2023-04-23 20:51:09 +0900

    [MEGA IT ACADEMY]Web Back-End Developer - SpringBoot eclipse SpringBoot - BoardDAO 작성(2023.04.23)

---
+++ example/src/main/java/com/megait/example/beans/dao/BoardDAO.java
@@ -0,0 +1,43 @@
+package com.megait.example.beans.dao;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.megait.example.beans.vo.BoardVO;
+import com.megait.example.mappers.BoardMapper;
+
+import lombok.RequiredArgsConstructor;
+
+@Repository  // @Component의 자식 annotation이며, DAO에 사용한다.
+@RequiredArgsConstructor
+public class BoardDAO {
+	@Autowired
+	private BoardMapper mapper;
+	
+	// 게시글 입력
+	public void register(BoardVO board) {
+		mapper.insertSelectKey_bno(board);
+	}
+	
+	// 게시글 가져오기
+	public BoardVO get(Long bno) {
+		return mapper.read(bno);
+	}
+	
+	// 게시글 삭제
+	public boolean remove(Long bno) {
+		return mapper.delete(bno) != 0;
+	}
+	
+	// 게시글 수정
+	public boolean modify(BoardVO board) {
+		return mapper.update(board) != 0;
+	}
+	
+	// 전체 게시글 가져오기
+	public List<BoardVO> getList() {
+		return mapper.getList();
+	}
+}
