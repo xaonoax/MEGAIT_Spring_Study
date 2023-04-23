6f0d380ef879e870f21ae519b2099bff8212ad62
Ji-0 <xaonoax@gmail.com>
2023-04-23 21:01:51 +0900

    [MEGA IT ACADEMY]Web Back-End Developer - SpringBoot eclipse SpringBoot - BoardMapperTest 작성(2023.04.22) - insert, read, delete, update 추가(2023.04.23)

--- example/src/test/java/com/megait/example/mappers/BoardMapperTest.java
+++ example/src/test/java/com/megait/example/mappers/BoardMapperTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.megait.example.beans.vo.BoardVO;
+
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest
@@ -18,5 +20,56 @@ public class BoardMapperTest {
 	@Test
 	public void testGetList() {
 		mapper.getList().forEach(board -> log.info(board.toString()));
+	}
+	
+//	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성");
+		board.setContent("새로 작성한 글의 내용");
+		board.setWriter("user01");
+		
+		mapper.insert(board);
+	}
+	
+//	@Test
+	public void testInsertSelectKey_bno() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성");
+		board.setContent("새로 작성한 글의 내용");
+		board.setWriter("user02");
+		
+		mapper.insertSelectKey_bno(board);
+	}
+	
+	@Test
+	public void testRead() {
+		long bno = 2L;
+		log.info(mapper.read(bno).toString());
+	}
+	
+//	@Test
+	public void testDelete() {
+		long bno = 3L;
+		log.info("[Delete count] " + mapper.delete(bno));
+	}
+	
+//	@Test
+	public void testUdate() {
+		Long bno = 3L;
+		
+		if (mapper.read(bno) != null) {
+			BoardVO board = new BoardVO();
+			// 실행 전에 존재하는 번호 확인
+			board.setBno(1L);
+			board.setTitle("수정된 제목");
+			board.setContent("수정된 내용");
+			board.setWriter("user00");
+		
+			log.info("[Update Count] " + mapper.update(board));
+		}
+		else {
+			log.info("[Update Count] No Such Board!!!");
+		}
 	}
 }
