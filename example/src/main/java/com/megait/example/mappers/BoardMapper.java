62b93cc285ae3295d00830aa15444b14c3583134
Ji-0 <xaonoax@gmail.com>
2023-04-23 20:52:39 +0900

    [MEGA IT ACADEMY]Web Back-End Developer - SpringBoot eclipse SpringBoot eclipse SpringBoot - BoardMapper 작성(2023.04.22) - BoardMapper 추가(2023.04.23)

--- example/src/main/java/com/megait/example/mappers/BoardMapper.java
+++ example/src/main/java/com/megait/example/mappers/BoardMapper.java
@@ -10,4 +10,19 @@ import com.megait.example.beans.vo.BoardVO;
 public interface BoardMapper {
 	// 게시글 목록
 	public List<BoardVO> getList();
+	
+	// 게시글 등록
+	public void insert(BoardVO board);
+	
+	// 게시글 등록(PK 가져오기)
+	public void insertSelectKey_bno(BoardVO board);
+	
+	// 게시글 상세보기(특정 게시글 정보)
+	public BoardVO read(Long bno);
+	
+	// 게시글 삭제
+	public int delete(Long bno);
+	
+	// 게시글 수정(수정이 완료된 건 수 리턴)
+	public int update(BoardVO board);
 }
