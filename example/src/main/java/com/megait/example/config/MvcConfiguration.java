46001225c8609424a154bb56705dfcfb72d8e3c9
Ji-0 <xaonoax@gmail.com>
2023-04-29 22:13:23 +0900

    [MEGA IT ACADEMY]Web Back-End Developer - SpringBoot eclipse SpringBoot - MvcConfiguration 작성(2023.04.29)

---
+++ example/src/main/java/com/megait/example/config/MvcConfiguration.java
@@ -0,0 +1,26 @@
+package com.megait.example.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfiguration implements WebMvcConfigurer {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry reg) {
+		reg.addResourceHandler("/js/**")
+		   .addResourceLocations("classpath:/static/js")
+		   .setCachePeriod(60 * 60 * 24 * 365);
+		reg.addResourceHandler("/css/**")
+		   .addResourceLocations("classpath:/static/css")
+		   .setCachePeriod(60 * 60 * 24 * 365);
+		reg.addResourceHandler("/img/**")
+		   .addResourceLocations("classpath:/static/img")
+		   .setCachePeriod(60 * 60 * 24 * 365);
+		reg.addResourceHandler("/font/**")
+		   .addResourceLocations("classpath:/static/font")
+		   .setCachePeriod(60 * 60 * 24 * 365);
+	}
+}
