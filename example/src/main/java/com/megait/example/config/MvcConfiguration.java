8a32cfa9b88acb56aa3da4272b5b956f673cbda1
Ji-0 <xaonoax@gmail.com>
2023-04-30 21:44:44 +0900

    [MEGA IT ACADEMY]Web Back-End Developer - SpringBoot eclipse SpringBoot - BoardController 작성(2023.04.29) - classpath 수정(2023.04.30)

--- example/src/main/java/com/megait/example/config/MvcConfiguration.java
+++ example/src/main/java/com/megait/example/config/MvcConfiguration.java
@@ -11,16 +11,16 @@ public class MvcConfiguration implements WebMvcConfigurer {
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry reg) {
 		reg.addResourceHandler("/js/**")
-		   .addResourceLocations("classpath:/static/js")
+		   .addResourceLocations("classpath:/static/js/")
 		   .setCachePeriod(60 * 60 * 24 * 365);
 		reg.addResourceHandler("/css/**")
-		   .addResourceLocations("classpath:/static/css")
+		   .addResourceLocations("classpath:/static/css/")
 		   .setCachePeriod(60 * 60 * 24 * 365);
 		reg.addResourceHandler("/img/**")
-		   .addResourceLocations("classpath:/static/img")
+		   .addResourceLocations("classpath:/static/img/")
 		   .setCachePeriod(60 * 60 * 24 * 365);
 		reg.addResourceHandler("/font/**")
-		   .addResourceLocations("classpath:/static/font")
+		   .addResourceLocations("classpath:/static/font/")
 		   .setCachePeriod(60 * 60 * 24 * 365);
 	}
 }
