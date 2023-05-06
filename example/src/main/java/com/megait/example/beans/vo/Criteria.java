bab437e9ec145b70090f556b4b1b9a5794360ca0
Ji-0 <xaonoax@gmail.com>
2023-05-06 21:23:01 +0900

    [MEGA IT ACADEMY]Web Back-End Developer - SpringBoot eclipse SpringBoot - Criteria 생성(2023.05.06)

---
+++ example/src/main/java/com/megait/example/beans/vo/Criteria.java
@@ -0,0 +1,32 @@
+package com.megait.example.beans.vo;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data
+public class Criteria {  // 게시글 검색의 기준
+	private int pageNum;  // 현재 몇 번째 페이지인가.
+	private int amount;  // 한 페이지에 보여줄 게시글의 갯수
+	private int limit;
+	private int offset;
+	
+	public Criteria() {
+		this(1, 10);
+//		pageNum = 1;
+//		amount = 10;
+	}
+	
+	public Criteria(int pageNum, int amount) {
+		this.pageNum = pageNum;
+		this.amount = amount;
+		this.limit = pageNum * amount;
+		this.offset = (pageNum - 1) * amount;
+	}
+	
+	public void setParam() {
+		this.limit = pageNum * amount;
+		this.offset = (pageNum - 1) * amount;
+	}
+}
