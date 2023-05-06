76dee4f7d1f9920ad84833466e1add52fd645720
Ji-0 <xaonoax@gmail.com>
2023-05-06 21:36:32 +0900

    [MEGA IT ACADEMY]Web Back-End Developer - SpringBoot eclipse SpringBoot - BoardDAOTest 작성(2023.04.23) - testGetList() 수정(2023.05.06)

--- example/src/test/java/com/megait/example/beans/dao/BoardDAOTest.java
+++ example/src/test/java/com/megait/example/beans/dao/BoardDAOTest.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.megait.example.beans.vo.BoardVO;
+import com.megait.example.beans.vo.Criteria;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -40,7 +41,8 @@ public class BoardDAOTest {
 	
 	@Test
 	public void testGetList() {
-		boardDAO.getList().forEach(board -> log.info("[testGetList] " + board.toString()));
+//		boardDAO.getList().forEach(board -> log.info("[testGetList] " + board.toString()));
+		boardDAO.getList(new Criteria(0, 5)).forEach(board -> log.info("---------------" + board.toString()));
 	}
 	
 //	@Test
